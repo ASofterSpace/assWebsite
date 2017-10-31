bbfbe211c751849bed991f0615fdc9c25454bb61
Moyaccercchi <moyaccercchi@hotmail.de>
2017-10-31 14:02:23 +0000

    Improve logo - now purple! \o/

--- index.php
+++ index.php
@@ -137,6 +137,9 @@
 					{{-- second round --}}
 					<a href="http://www.egscc.esa.int/"><img id="expcarouselFirstImgSecondRound" alt="EGS-CC logo" src="pics/exp_egscc.png" style="height: 50px; width: 278px;" /></a>
 					<a href="https://nasa.github.io/openmct/"><img alt="OpenMCT logo" src="pics/exp_openmct.png" style="height: 50px; width: 241px;" /></a>
+					{{-- for now, as there are so few of them, we also need a third round --}}
+					<a href="http://www.egscc.esa.int/"><img alt="EGS-CC logo" src="pics/exp_egscc.png" style="height: 50px; width: 278px;" /></a>
+					<a href="https://nasa.github.io/openmct/"><img alt="OpenMCT logo" src="pics/exp_openmct.png" style="height: 50px; width: 241px;" /></a>
 				</div>
 			</div>
 		</section>
