901b4339df9c9f8bfd90762ff1c31dceb0a7d3d8
Moyaccercchi <moyaccercchi@hotmail.de>
2017-11-25 18:33:17 +0000

    Added silly privacy page for the UniversalConverter

---
+++ privacy/universalconverter.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>Privacy Policy for the UniversalConverter</title>
+	</head>
+	
+	<body>
+		The UniversalConverter app includes the feature to use the flashlight of the Android device to output generated Morse code.<br>
+        To access the flashlight to perform this action, the camera needs to be accessed, as the flashlight itself is technically part of the camera subsystem of the Android device.<br>
+        Therefore, the UniversalConverter app requires access to the camera. As it is only interested in the flashlight, no data is collected from the camera, and no data is stored or transmitted anywhere.<br>
+        Also, no data regarding the use of the flashlight is collected, stored or transmitted anywhere.<br>
+        The user has the full right to request the withdrawal of consent and deletion of all data by contacting e.g. <a href="mailto:moya@asofterspace.com">moya@asofterspace.com</a> (which however will result in no action at all, as no data is collected in the first place.)<br>
+        Sincerely,<br>
+        Tom Moya Schiller, a softer space
+	</body>
+
+</html>
