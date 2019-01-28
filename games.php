650e9fd0c14c5b02eef42fa145b18474d62fbdbd
Moya <moyaccercchi@hotmail.de>
2019-01-28 09:13:59 +0100

    Add vrCade video

--- games.php
+++ games.php
@@ -13,4 +13,23 @@
 	</div>
 @include(sectionend.php)
 
+@include(sectionstart.php)
+	<h2>vrCade</h2>
+
+	<div class="content">
+		Our VR Arcade hall - called vrCade - is currently under development.<br>
+		The idea is that several pinball machines and simple arcade machines will be available such that you can enjoy the fun of an entire arcade hall without having to leave your living room. :)
+	</div>
+
+	<div class="content">
+		Here is a short preview on which you can see the first pinball machine, Flipper QnD:
+	</div>
+
+	<div class="content">
+		<div style="position:relative;padding-top:56.25%;">
+			<iframe src="https://www.youtube.com/embed/7mkDv6NCrKQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;"></iframe>
+		</div>
+	</div>
+@include(sectionend.php)
+
 @include(bottom.php)
