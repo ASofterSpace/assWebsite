047404df5ce954af05ffaccc20024cab86ef1d7d
Tom Moya Schiller <moya@asofterspace.com>
2019-04-10 16:19:28 +0200

    Add reference graphs thesis paper

--- science/index.php
+++ science/index.php
@@ -13,7 +13,7 @@
 	<h2>@content(ekse_headline)</h2>
 
 	<div class="content">
-			<a href="/science/papers/ekse.pdf" class="paperpreview">
+			<a href="/science/papers/ekse.pdf" target="_blank" class="paperpreview">
 				<img src="/science/papers/ekse_cover.png" />
 			</a>
 			<div class="papertable">
@@ -33,7 +33,7 @@
 						</tr>
 						<tr>
 							<td>@content(size):</td>
-							<td>A4</td>
+							<td>US-Letter</td>
 						</tr>
 						<tr>
 							<td>@content(author):</td>
@@ -51,6 +51,55 @@
 						<tr>
 							<td style="vertical-align: bottom;">@content(last_updated):</td>
 							<td>@content(ekse_update_date)</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+	</div>
+@include(sectionend.php)
+
+<a id="reference_graphs"></a>
+@include(sectionstart.php)
+	<h2>@content(reference_graphs_headline)</h2>
+
+	<div class="content">
+			<a href="/science/papers/reference_graphs.pdf" target="_blank" class="paperpreview">
+				<img src="/science/papers/reference_graphs_cover.png" />
+			</a>
+			<div class="papertable">
+				<table>
+					<tbody>
+						<tr>
+							<td style="min-width: 6.5em;">@content(resource_kind):</td>
+							<td>@content(masters_thesis)</td>
+						</tr>
+						<tr>
+							<td>@content(file_type):</td>
+							<td><a class="plain" href="/science/papers/reference_graphs.pdf" target="_blank">PDF</a></td>
+						</tr>
+						<tr>
+							<td>@content(pages):</td>
+							<td>116</td>
+						</tr>
+						<tr>
+							<td>@content(size):</td>
+							<td>A4</td>
+						</tr>
+						<tr>
+							<td>@content(author):</td>
+							<td><a class="plain" href="mailto:moya@asofterspace.com">Tom Moya Schiller</a></td>
+						</tr>
+						<tr>
+							<td>@content(status):</td>
+							<td>@content(reference_graphs_status)</td>
+						</tr>
+						<tr>
+							<td>URI:</td>
+							<td><a href="http://hdl.handle.net/1946/23636" target="_blank">1946/23636</a></td>
+						</tr>
+						<tr>
+							<td style="vertical-align: bottom;">@content(last_updated):</td>
+							<td>@content(reference_graphs_update_date)</td>
 						</tr>
 					</tbody>
 				</table>
