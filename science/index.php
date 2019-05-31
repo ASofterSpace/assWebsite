05f537c167c68cecc957c84d8365648f7966e8d7
Tom Moya Schiller <moya@asofterspace.com>
2019-05-31 21:40:27 +0200

    Add Rewinding the Clock paper

--- science/index.php
+++ science/index.php
@@ -13,6 +13,47 @@
 	<h2>@content(rewinding_the_clock_headline)</h2>
 
 	<div class="content">
+		<a href="/science/papers/rewinding_the_clock.pdf" target="_blank" class="paperpreview">
+			<img src="/science/papers/rewinding_the_clock_cover.png" />
+		</a>
+		<div class="papertable">
+			<table>
+				<tbody>
+					<tr>
+						<td style="min-width: 6.5em;">@content(resource_kind):</td>
+						<td>@content(conference_paper)</td>
+					</tr>
+					<tr>
+						<td>@content(file_type):</td>
+						<td><a class="plain" href="/science/papers/rewinding_the_clock.pdf" target="_blank">PDF</a></td>
+					</tr>
+					<tr>
+						<td>@content(pages):</td>
+						<td>15</td>
+					</tr>
+					<tr>
+						<td>@content(size):</td>
+						<td>US-Letter</td>
+					</tr>
+					<tr>
+						<td>@content(author):</td>
+						<td><a class="plain" href="mailto:moya@asofterspace.com">Tom Moya Schiller</a></td>
+					</tr>
+					<tr>
+						<td>@content(status):</td>
+						<td>@content(rewinding_the_clock_paper_status)</td>
+						{{-- <td>This is a preview of unpublished work.</td> --}}
+					</tr>
+					<tr>
+						<td style="vertical-align: bottom;">@content(last_updated):</td>
+						<td>@content(rewinding_the_clock_update_date)</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<div class="content">
 		<a href="/science/papers/rewinding_the_clock.pptx" target="_blank" class="paperpreview">
 			<img src="/science/papers/rewinding_the_clock_presentation_cover.png" />
 		</a>
