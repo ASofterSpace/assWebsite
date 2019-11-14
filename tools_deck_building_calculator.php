c2aa03fd6b1428165ffd5b7016818838101e93a2
Tom Moya Schiller <moya@asofterspace.com>
2019-11-15 00:04:09 +0100

    Translate queen, king, cat and dog

--- tools_deck_building_calculator.php
+++ tools_deck_building_calculator.php
@@ -8,10 +8,10 @@
 </div>
 
 <div class="content">
-	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-0" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-0" onchange="dBC.docalculate()" value="Queen"></input>.</div>
-	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-1" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-1" onchange="dBC.docalculate()" value="King"></input>.</div>
-	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-2" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-2" onchange="dBC.docalculate()" value="Cat"></input>.</div>
-	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-3" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-3" onchange="dBC.docalculate()" value="Dog"></input>.</div>
+	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-0" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-0" onchange="dBC.docalculate()" value="@content(tools_deck_building_calculator_queen)"></input>.</div>
+	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-1" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-1" onchange="dBC.docalculate()" value="@content(tools_deck_building_calculator_king)"></input>.</div>
+	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-2" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-2" onchange="dBC.docalculate()" value="@content(tools_deck_building_calculator_cat)"></input>.</div>
+	<div>@content(tools_deck_building_calculator_you_mark) <input type="text" id="dBC-mark-amount-3" onchange="dBC.docalculate()" onkeyup="dBC.docalculate()" value="4"></input> @content(tools_deck_building_calculator_cards_as) <input type="text" id="dBC-mark-3" onchange="dBC.docalculate()" value="@content(tools_deck_building_calculator_dog)"></input>.</div>
 </div>
 
 <div class="content">
