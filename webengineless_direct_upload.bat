afa5d213b5564cd3861514bb0747031ed598a2e4
Tom Moya Schau <moya@asofterspace.com>
2020-09-22 03:33:08 +0200

    Add direct upload script

---
+++ webengineless_direct_upload.bat
@@ -0,0 +1,3 @@
+cd temp
+C:\home\system\psftp\psftp.exe ubuntu@185.12.7.253 -i C:\home\prog\webdev\sec\cloudsigmaprivate.ppk -b en.bat
+C:\home\system\psftp\psftp.exe ubuntu@185.12.7.253 -i C:\home\prog\webdev\sec\cloudsigmaprivate.ppk -b de.bat
