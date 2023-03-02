0bac4cdcd50d141dd9feeb555e3d0f0f0127b1c0
Tom Moya Schau <moya@asofterspace.com>
2023-03-02 09:56:20 +0100

    Add fem streik

--- expcarousel.php
+++ expcarousel.php
@@ -1,7 +1,8 @@
 
 <a href="https://thebitbybit.com/" target="_blank"><img alt="Bit by Bit logo" src="pics/exp_bitbybit.png"/></a>
 <a href="https://supervision.earth/" target="_blank"><img alt="SuperVision Earth logo" src="pics/exp_sve.png"/></a>
-<a href="http://skyhook.is/" target="_blank"><img alt="Skyhook logo" src="pics/exp_skyhook.png"/></a>
+<a href="https://skyhook.is/" target="_blank"><img alt="Skyhook logo" src="pics/exp_skyhook.png"/></a>
+<a href="https://feministischerstreik.org/" target="_blank"><img alt="Feministischer Streik logo" src="pics/exp_fem_streik.png"/></a>
 <a href="https://www.right-basedonscience.de/" target="_blank"><img alt="Right based on science logo" src="pics/exp_right.png"/></a>
 <a href="https://nasa.github.io/openmct/" target="_blank"><img alt="OpenMCT logo" src="pics/exp_openmct.png"/></a>
 <a href="http://www.bluebirdcargo.com/" target="_blank"><img alt="Bluebird Cargo logo" src="pics/exp_bluebird.png"/></a>
