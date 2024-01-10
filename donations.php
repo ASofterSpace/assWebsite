c53ce59ab20ad4b989f856060d41cd011609bdd4
Moya <moya@asofterspace.com>
2024-01-10 14:05:28 +0100

    Add donation

--- donations.php
+++ donations.php
@@ -13,6 +13,14 @@
 @include(sectionend.php)
 
 @include(sectionstart.php)
+	<h1>@content(donations_headline_2024)</h1>
+
+	<div class="content">
+		@content(donations_2024_individuals)
+	</div>
+@include(sectionend.php)
+
+@include(sectionstart.php)
 	<h1>@content(donations_headline_2023)</h1>
 
 	<div class="content">
