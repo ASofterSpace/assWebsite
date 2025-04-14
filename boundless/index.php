fc23beac8e541034e19ae15b5b1018fcbe9c3e6e
Moya <moya@asofterspace.com>
2025-04-14 10:48:39 +0200

    Add Boundless signup page

---
+++ boundless/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="@content(lang)">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>Boundless - Tentacles Below!</title>
+
+		{{-- SEO keywords (not terribly important though) --}}
+		<meta name="keywords" content="Boundless, Comic, Pirates, Polyamory">
+
+		{{-- description should be 150 characters long; not more than 160! --}}
+		<meta name="description" content="“Boundless - Tentacles Below!” is the name of an adventure-filled comicbook full of polyamorous pirates. Tentaclive release date: summer 2025.">
+
+		<style>
+			body {
+				padding: 5pt 20% 5pt 20%;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Boundless - Tentacles Below!</h1>
+		<p>“Boundless - Tentacles Below!” is the name of an adventure-filled comicbook full of polyamorous pirates. Tentaclive release date: summer 2025.</p>
+		<p>If you would like to receive occasional information about Boundless - Tentacles Below! in the future, please enter your email address below:</p>
+		<form action="https://asofterspace.com/boundless/signup" method="post">
+			<div>
+				<label for="email">Your email address:</label>
+				<input type="text" id="email" name="email" required />
+			</div>
+			<div>
+				<input type="submit" value="Subscribe!" />
+			</div>
+		</form>
+		<p>Your Boundless team</p>
+		<p>&nbsp;</p>
+		<p>(You can reach out to us via <a target="_blank" href="mailto:boundless@asofterspace.com">boundless@asofterspace.com</a> - we are looking forward to your emails!)</p>
+	</body>
+</html>
